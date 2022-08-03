eccbde3f223f1d386dd6ac56e91611277e51b527
ktg441 <kiran.t.goel@gmail.com>
2022-08-04 00:10:14 +0100

    Update text to indicate I'm working on the site again. Add favicon and picture of my dog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import dog from './assets/dog.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-100 h-100 px-4 pt-2">
+      <h1>Uh-oh! This site is currently down for maintenance</h1>
+      <p>I am actively updating my site to a snazzy 2.0 version. While you wait, enjoy a picture of my dog!</p>
+      <img src={dog} width={400} height={300} alt={'My dog'}/>
     </div>
   );
 }
